756de88f105c5c470b13b99525f56be591ca53e3
Godstime Agholor <agholorgodstime18@gmail.com>
2022-09-01 21:06:52 +0100

    done with crew desktop design

--- src/compnents/Crew.jsx
+++ src/compnents/Crew.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
+import CrewSlider from "./CrewSlider";
 
 function Crew() {
   return (
@@ -10,21 +11,7 @@ function Crew() {
           <span>02</span> Meet your crew
         </h5>
       </div>
-      <div className="content crew-con">
-        <div className="info">
-          <div className="right"></div>
-          <div className="crew-det">
-            <h4>Commander</h4>
-            <h3>Douglas Hurley</h3>
-            <p>
-              Douglas Gerald Hurley is an American engineer, former Marine Corps
-              pilot and former NASA astronaut. He launched into space for the
-              third time as commander of Crew Dragon Demo-2.
-            </p>
-          </div>
-        </div>
-        <div className="blank"></div>
-      </div>
+      <CrewSlider />
     </div>
   );
 }
---
+++ src/compnents/CrewSlider.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Slider from "react-slick";
+import data from "../dataSlider";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ImgOne from "../assets/crew/image-douglas-hurley.png";
+import ImgTwo from "../assets/crew/image-mark-shuttleworth.png";
+import ImgThree from "../assets/crew/image-victor-glover.png";
+import ImgFour from "../assets/crew/image-anousheh-ansari.png";
+
+const Card = ({ name, title, about, image }) => {
+  return (
+    <div className="content crew-con">
+      <div className="info">
+        {/* <div className="right"></div> */}
+        <div className="crew-det">
+          <h4>{title}</h4>
+          <h3>{name}</h3>
+          <p>{about}</p>
+        </div>
+      </div>
+      <div className="crew-img">
+        <img src={image} alt="/" />
+      </div>
+    </div>
+  );
+};
+
+function CrewSlider() {
+  const settings = {
+    dots: true,
+    arrows: false,
+  };
+  return (
+    <>
+      <Slider {...settings}>
+        {data.map((obj, index) => (
+          <Card {...obj} key={index} />
+        ))}
+      </Slider>
+    </>
+  );
+}
+
+export default CrewSlider;
